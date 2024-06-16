2e7b65daf7a48025ac40717bf7c907eba3263354
DESKTOP-734T2BT\soeun <colacider97@gmail.com>
2024-06-17 04:13:33 +0900

    2024.06.17 commit

---
+++ httpServer.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.listen(8080, function () {
+    console.log('server ready...');
+});
+
+app.get('/signin', function(req, res){
+    res.sendFile(__dirname+"/sign-in.html");
+});
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname+"/index.html");
+});
+
+app.use(express.static(path.join(__dirname, '..', 'NodeAssignment')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'NodeAssignment', 'index.html'));
+});
+
+// const http = require('http');
+
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/html');
+//     res.end('<h1>hello world</h1>');
+// });
+
+// server.listen(3000, '127.0.0.1', () => {
+//     console.log('server ready...');
+// })
